cf3f863134fcc9bc33379e28d16112390a76a7c6
TobiasAAM <tobias.fischer@artsalliancemedia.com>
2013-08-02 09:14:42 +0100

    Some first API investigation - Created settings-template.json - Access API, authorise with user/password, get schedule - Print schedule

--- tms_lobby_display.py
+++ tms_lobby_display.py
@@ -1,11 +1,35 @@
 '''
 Created on 1 Aug 2013
 
-@author: tfischer
+@author: Tobias Fischer
 '''
 
+import json
+import requests
+import os
+
 def main():
-    print 'TMS lobby display'
+    #read config file
+    try:
+        config = json.load(open(os.path.join(os.path.dirname(__file__), u'settings.json'), u'r'))
+    except IOError:
+        raise IOError(u'The settings.json file does not exist')
     
-if __name__ == '__main__':
+    url = config[u'coreAPI'] + u'monitoring/info'
+    headers = {u'content-type': u'application/json'}
+    
+    payload = {'username':config[u'username'], 'password':config[u'password']}    
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    device_info = json.loads(response.text)
+    
+    url = config[u'coreAPI'] + u'scheduling/schedule'
+    payload = {'username':config[u'username'], 'password':config[u'password'], u'start_time': u'2013-08-01 11:00:00', u'end_time': u'2013-08-01 13:59:59'}
+    
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    schedule = json.loads(response.text)
+    for item in schedule['data']:
+        #print device_info['data'][schedule['data'][item]['screen_uuid']]
+        print schedule['data'][item]
+    
+if __name__ == u'__main__':
     main()
